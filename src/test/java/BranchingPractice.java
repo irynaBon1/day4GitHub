7104bf9d5e3e9989a881f39b59c4040c35296c6a
IrynaBonchak <beatle600@gmail.com>
2021-05-25 18:24:01 -0500

    Share on git hub

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,9 @@
 public class BranchingPractice {
     public static void main(String[] args) {
-
+//created new project
+        //enable version control
+        //added .gitignore with .idea, target folder and .iml file
+        // made initial commit by keeping  trac of all files
+        // share on git hub
     }
 }
